51ab3379ac89f22e3eec3a3bd651b91307ebcd24
tracer-ics2017 <tracer@njuics.org>
2020-06-10 06:54:35 -0700

    >  compile 1410576 ljl Linux ubuntu 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:44:23 UTC 2020 i686 athlon i686 GNU/Linux  06:54:35 up 10:34,  1 user,  load average: 0.04, 0.05, 0.08 d5caf75e5a3c47b548b8f53caa7c7c664e5ce13

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -2,7 +2,12 @@
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
+extern void ramdisk_read(void *buf,off_t offset,size_t len);
+extern size_t get_ramdisk_size();//外部声明
+
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  //TODO();
+  ramdisk_read(DEFAULT_ENTRY,0,get_ramdisk_size());//加载用户程序到正确的位置
   return (uintptr_t)DEFAULT_ENTRY;
 }
+
