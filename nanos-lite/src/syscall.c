0490852fe47f388e136e98b6ee03c14e6ba3da8e
tracer-ics2017 <tracer@njuics.org>
2020-06-13 03:43:21 -0700

    >  run 1410576 ljl Linux ubuntu 4.15.0-106-generic #107~16.04.1-Ubuntu SMP Thu Jun 4 15:39:45 UTC 2020 i686 athlon i686 GNU/Linux  03:43:21 up  6:16,  1 user,  load average: 0.39, 0.16, 0.12 8e2e5f0e22e095fd628b4d5af2d6667ecc875d53

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -9,16 +9,33 @@ void sys_exit(int a) {
   _halt(a);
 }
 
+int sys_write(int fd,void* buf,size_t len) {
+  if(fd==1||fd==2) {//fd为1或2则执行
+    char c;
+    for(int i=0;i<len;i++) {
+      memcpy(&c,buf+i,1);
+      _putc(c);
+    }
+    return len;
+  }
+  else {//否则返回错误
+    panic("Unhandled fd=%d in sys_write",fd);
+  }
+  return -1;
+}
+
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
+  //获取系统调用参数
   a[0] = SYSCALL_ARG1(r);
   a[1] = SYSCALL_ARG2(r);
   a[2] = SYSCALL_ARG3(r);
   a[3] = SYSCALL_ARG4(r);
 
-  switch (a[0]) {
+  switch (a[0]) {//判断系统调用号
     case SYS_none: SYSCALL_ARG1(r)=sys_none();break;
     case SYS_exit: sys_exit(a[1]); break;
+    case SYS_write: SYSCALL_ARG1(r)=sys_write(a[1],(void*)a[2],a[3]);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
