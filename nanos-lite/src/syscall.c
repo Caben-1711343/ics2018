47f8264301db352e0ecba2a0d663736bc6ff6fc8
tracer-ics2017 <tracer@njuics.org>
2020-06-13 21:35:57 -0700

    >  run 1410576 ljl Linux ubuntu 4.15.0-106-generic #107~16.04.1-Ubuntu SMP Thu Jun 4 15:39:45 UTC 2020 i686 athlon i686 GNU/Linux  21:35:57 up  1:01,  1 user,  load average: 0.55, 0.30, 0.38 6e8308ad35274379d4111d64dc13f37dffed1c4

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -12,7 +12,7 @@ void sys_exit(int a) {
 int sys_write(int fd,void* buf,size_t len) {
   if(fd==1||fd==2) {//fd为1或2则执行
     char c;
-    Log("buffer:%s",(char*)buf);//用于比较
+    Log("buffer: %s",(char*)buf);//用于比较
     for(int i=0;i<len;i++) {
       memcpy(&c,buf+i,1);
       _putc(c);
@@ -26,6 +26,7 @@ int sys_write(int fd,void* buf,size_t len) {
 }
 
 int sys_brk(int addr) {
+  Log("SSSS");
   return 0;
 }
   
