0369c64eb187ff5182586a1da636850259137787
tracer-ics2017 <tracer@njuics.org>
2020-06-14 04:41:41 -0700

    >  run 1410576 ljl Linux ubuntu 4.15.0-106-generic #107~16.04.1-Ubuntu SMP Thu Jun 4 15:39:45 UTC 2020 i686 athlon i686 GNU/Linux  04:41:41 up  6:57,  1 user,  load average: 0.11, 0.09, 0.03 8b1db24417729c4531582f4a9d4a0c9198291de

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,4 +1,5 @@
 #include "common.h"
+#include "fs.h"
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
@@ -7,7 +8,11 @@ extern size_t get_ramdisk_size();//外部声明
 
 uintptr_t loader(_Protect *as, const char *filename) {
   //TODO();
-  ramdisk_read(DEFAULT_ENTRY,0,get_ramdisk_size());//加载用户程序到正确的位置
+  //ramdisk_read(DEFAULT_ENTRY,0,get_ramdisk_size());//加载用户程序到正确的位置
+  int fd=fs_open(filename,0,0);//打开文件
+  Log("filename=%s,fd=%d",filename,fd);
+  fs_read(fd,DEFAULT_ENTRY,fs_filesz(fd));//读文件
+  fs_close(fd);//关闭文件
   return (uintptr_t)DEFAULT_ENTRY;
 }
 
