63b7d910746779ce7c687bfe3d70054db6d7237c
tracer-ics2017 <tracer@njuics.org>
2020-06-17 10:13:09 -0700

    >  compile 1410576 ljl Linux ubuntu 4.15.0-106-generic #107~16.04.1-Ubuntu SMP Thu Jun 4 15:39:45 UTC 2020 i686 athlon i686 GNU/Linux  10:13:09 up 40 min,  1 user,  load average: 0.58, 0.40, 0.56 ddcf6cc5666233416cba95b33ab95dc62daae236

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -248,6 +248,8 @@ void exec_wrapper(bool print_flag) {
 #ifdef DIFF_TEST
   uint32_t eip = cpu.eip;
 #endif
+
+  update_eip();
   if(cpu.INTR & cpu.eflags.IF) {//开中断并接收到中断信号
     cpu.INTR=false;
     extern void raise_intr(uint8_t NO,vaddr_t ret_addr);
@@ -255,7 +257,7 @@ void exec_wrapper(bool print_flag) {
     update_eip();
   }
 
-  //update_eip();
+
 
 #ifdef DIFF_TEST
   void difftest_step(uint32_t);
