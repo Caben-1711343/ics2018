c6649371478f12b0a838310d289e4722e6d59037
tracer-ics2017 <tracer@njuics.org>
2020-06-17 07:38:29 -0700

    >  compile 1410576 ljl Linux ubuntu 4.15.0-106-generic #107~16.04.1-Ubuntu SMP Thu Jun 4 15:39:45 UTC 2020 i686 athlon i686 GNU/Linux  07:38:29 up  7:10,  1 user,  load average: 0.39, 0.23, 0.34 80d3bc6f302d9190dfb96b366897d3635cc12b1

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -12,6 +12,7 @@ typedef struct {
 #define EXW(ex, w)         {NULL, concat(exec_, ex), w}
 #define EX(ex)             EXW(ex, 0)
 #define EMPTY              EX(inv)
+#define TIME_IRQ 32        //时钟中断
 
 static inline void set_width(int width) {
   if (width == 0) {
@@ -246,7 +247,11 @@ void exec_wrapper(bool print_flag) {
 #ifdef DIFF_TEST
   uint32_t eip = cpu.eip;
 #endif
-
+  if(cpu.INTR & cpu.eflags.IF) {//开中断且接收到中断信号
+    cpu.INTR=false;
+    extern void raise_intr(uint8_t NO,vaddr_t ret_addr);
+    raise_intr(TIME_IRQ,cpu.eip);
+  }
   update_eip();
 
 #ifdef DIFF_TEST
