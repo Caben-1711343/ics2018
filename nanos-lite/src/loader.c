7212cc96936811ba725649e212e20ba45a4bb80c
tracer-ics2017 <tracer@njuics.org>
2020-06-17 04:13:17 -0700

    >  run 1410576 ljl Linux ubuntu 4.15.0-106-generic #107~16.04.1-Ubuntu SMP Thu Jun 4 15:39:45 UTC 2020 i686 athlon i686 GNU/Linux  04:13:17 up  5:40,  1 user,  load average: 0.00, 0.08, 0.29 eed2f353e539e5e1da6c01225fb4239aa3e1072d

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -6,25 +6,24 @@
 
 extern void ramdisk_read(void *buf,off_t offset,size_t len);
 extern size_t get_ramdisk_size();//外部声明
+extern void* new_page(void);
 
 uintptr_t loader(_Protect *as, const char *filename) {
   //TODO();
   //ramdisk_read(DEFAULT_ENTRY,0,get_ramdisk_size());//加载用户程序到正确的位置
   int fd=fs_open(filename,0,0);//打开文件
-  Log("filename=%s,fd=%d",filename,fd);
+  //Log("filename=%s,fd=%d",filename,fd);
   //fs_read(fd,DEFAULT_ENTRY,fs_filesz(fd));//读文件
-  int size=fs_filesz(fd);
-  int pagenum=size/PGSIZE;
-  if(size%PGSIZE!=0) {
-    pagenum++;
-  }
-  void *pa=NULL;
-  void *va=DEFAULT_ENTRY;
-  for(int i=0;i<pagenum;i++) {
+  size_t len=fs_filesz(fd);//获取文件大小
+  void* pa;
+  void* va;
+  Log("filename=%s,fd=%d",filename,fd);
+  void* end=DEFAULT_ENTRY+len;
+  //从文件中按页读取，不足一页单独处理
+  for(va=DEFAULT_ENTRY;va<end;va+=PGSIZE) {
     pa=new_page();
     _map(as,va,pa);
-    fs_read(fd,pa,PGSIZE);
-    va+=PGSIZE;
+    fs_read(fd,pa,(end-va)<PGSIZE ? (end-va) : PGSIZE);
   }
   fs_close(fd);//关闭文件
   return (uintptr_t)DEFAULT_ENTRY;
