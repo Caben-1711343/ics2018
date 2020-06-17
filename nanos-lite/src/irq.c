f71fab7f1e0ef2b78c3424fbeab755ccb95e439e
tracer-ics2017 <tracer@njuics.org>
2020-06-17 04:52:40 -0700

    >  run 1410576 ljl Linux ubuntu 4.15.0-106-generic #107~16.04.1-Ubuntu SMP Thu Jun 4 15:39:45 UTC 2020 i686 athlon i686 GNU/Linux  04:52:40 up  6:19,  1 user,  load average: 0.85, 1.08, 0.92 8e06697e840c91f9bf9783c1da47ca458546ded

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,6 +5,9 @@ static _RegSet* do_event(_Event e, _RegSet* r) {
   switch (e.event) {
     case _EVENT_SYSCALL://调用do_syscall()函数
       return do_syscall(r);
+    case _EVENT_TRAP:
+      printf("Receive an event trap!!!\n");
+      return NULL;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
