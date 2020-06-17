17b77ddc056d8f536c9ca521daa0842a9d0aa94e
tracer-ics2017 <tracer@njuics.org>
2020-06-17 09:36:50 -0700

    >  compile 1410576 ljl Linux ubuntu 4.15.0-106-generic #107~16.04.1-Ubuntu SMP Thu Jun 4 15:39:45 UTC 2020 i686 athlon i686 GNU/Linux  09:36:50 up 4 min,  1 user,  load average: 1.44, 1.10, 0.51 d42eba8abbb7275562ec09823d9c519f3c2f4de3

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -5,29 +5,29 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
+	// 将eflags、cs、eip入栈
+	memcpy(&t1,&cpu.eflags,sizeof(cpu.eflags));
+	rtl_li(&t0,t1);
+	rtl_push(&t0);
+	rtl_push(&cpu.cs);
+	rtl_li(&t0,ret_addr);
+	rtl_push(&t0);
+	// 根据中断号找到门描述符地址
+	vaddr_t gate_addr=cpu.idtr.base+NO*sizeof(GateDesc);
+	// 检验合法性
+	assert(gate_addr<=cpu.idtr.base+cpu.idtr.limit);
+  	// 找到中断处理程序的地址
+  	uint32_t low=vaddr_read(gate_addr,2);
+  	uint32_t high=vaddr_read(gate_addr+6,2);
+  	uint32_t target_addr=(high<<16)+low;
 
-  //TODO();
-  memcpy(&t1,&cpu.eflags,sizeof(cpu.eflags));
-  rtl_li(&t0,t1);
-  rtl_push(&t0);
-  cpu.eflags.IF=0;//关中断
-  rtl_push(&cpu.cs);
-  rtl_li(&t0,ret_addr);
-  rtl_push(&t0);
-  
-  //寻找IDT对应NO的门描述符首地址
-  vaddr_t gate_addr=cpu.idtr.base+NO*sizeof(GateDesc);//根据索引找到门描述符
-  assert(gate_addr<=cpu.idtr.base+cpu.idtr.limit);
-  
-  //读取偏移量
-  uint32_t off_15_0=vaddr_read(gate_addr,2);
-  uint32_t off_32_16=vaddr_read(gate_addr+sizeof(GateDesc)-2,2);
-  uint32_t target_addr=(off_32_16<<16)+off_15_0;
 #ifdef DEBUG
-  Log("target_addr=0x%x",target_addr);
+  	Log("target_addr: 0x%x",target_addr);
 #endif
-  decoding.is_jmp=1;
-  decoding.jmp_eip=target_addr;
+  	// 设置跳转
+  	decoding.is_jmp=1;
+  	decoding.jmp_eip=target_addr;
+  // TODO();
 }
 
 void dev_raise_intr() {
